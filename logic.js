a4e576caef8b7b9e35d81dcad04e2424510345d5
Kenneth Nguyen <n.kennethcs@gmail.com>
2019-07-26 23:52:03 -0700

    Implement addEventListeners to display values of each number button and to clear the display

--- logic.js
+++ logic.js
@@ -13,3 +13,15 @@ const operate = (operator,first_number,second_number) => {
          : operator === "Divide" ? (divide(first_number,second_number))
          : ''
 }
+
+const display = document.querySelector('#display')
+
+var number = document.getElementsByClassName('number')
+
+for(let i = 0; i < number.length; i++) {
+        number[i].addEventListener('click', () => display.innerHTML += number[i].innerHTML)
+}
+
+var operator = document.getElementsByClassName('operator')
+
+document.querySelector('#clear').addEventListener('click', () => display.innerHTML = '0')
