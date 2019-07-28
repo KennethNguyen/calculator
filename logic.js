e6728019395b2e6806684a6754660815d29f9fb6
Kenneth Nguyen <n.kennethcs@gmail.com>
2019-07-27 23:53:48 -0700

    Remove 'clear' button's class attribute for compatability in javascript code and adjust color

--- logic.js
+++ logic.js
@@ -14,14 +14,42 @@ const operate = (operator,first_number,second_number) => {
          : ''
 }
 
-const display = document.querySelector('#display')
+
+var first_number_input = 0
+var second_number_input = 0
+var current_number_input = ""
+var switch_input = true // if true, first_number_input is being manipulated, else second_number_input is being manipulated
+
+
+
+var display = document.querySelector('#display')
 
 var number = document.getElementsByClassName('number')
 
-for(let i = 0; i < number.length; i++) {
-        number[i].addEventListener('click', () => display.innerHTML += number[i].innerHTML)
+function updateDisplay(input) {
+        current_number_input += input
+        display.innerHTML = current_number_input
 }
+
+for(let i = 0; i < number.length; i++) {
+        number[i].addEventListener('click', function() {
+                updateDisplay(number[i].innerHTML)
+        })
+}
+
+document.querySelector('#clear').addEventListener('click', function() {
+        display.innerHTML = 0
+        first_number_input = 0
+        second_number_input = 0
+        current_number_input = 0
+})
 
 var operator = document.getElementsByClassName('operator')
 
-document.querySelector('#clear').addEventListener('click', () => display.innerHTML = '0')
+for(let j = 0; j < operator.length; j++) {
+        operator[i].addEventListener('click', function() {
+                // 1. get second number
+                // 2. execute operator 
+                // 3. display results
+        })
+}
